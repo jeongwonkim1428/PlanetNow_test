f9a52d13de940105544bdc57b37c4e738c9ec750
sxxnxhy <seunghyu823@gmail.com>
2025-01-23 13:06:45 +0900

    Initial commit

---
+++ src/main/java/com/application/planetnow/PlanetNowApplication.java
@@ -0,0 +1,13 @@
+package com.application.planetnow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlanetNowApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PlanetNowApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/application/planetnow/PlanetNowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.application.planetnow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlanetNowApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
