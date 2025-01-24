44c24666622c46f7ca57b0a3fb80c8b062f8885f
sxxnxhy <seunghyu823@gmail.com>
2025-01-24 11:49:48 +0900

    refactor: html 수정

--- src/main/java/com/application/planetnow/follow/FollowController.java
+++ src/main/java/com/application/planetnow/follow/FollowController.java
@@ -1,10 +1,15 @@
 package com.application.planetnow.follow;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/follow")
 public class FollowController {
 
+    @GetMapping("/follower-list")
+    public String followerList() {
+        return "/user/user-list";
+    }
 }
